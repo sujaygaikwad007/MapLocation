b2e68f76838798d27fc64a1efe75e959765d3b83
Aniket Patil <aniket.p@reapmind.com>
2023-11-21 13:11:51 +0530

    Initial Commit

---
+++ MapLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapLocation
+//
+//  Created by Aniket Patil on 21/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
